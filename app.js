9a532feb54c2a6b171e11b89825e14b1c11a8d7c
Jose Octavio de Castro Neves Jr <joc@fullsquad.com.br>
2019-07-05 11:12:15 -0300

    basic setup - route zero

---
+++ app.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const path = require('path');
+const indexRoutes = require('./routes/index');
+
+
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('connected to mongoDB');
+});
+
+app.use(morgan('dev'));
+
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRoutes);
+
+app.listen(process.env.PORT, () => console.log(`server is running on port ${process.env.PORT}`));
